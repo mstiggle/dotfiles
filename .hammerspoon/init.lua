91841cd5d57f614af6c79ba457cb77cfb68e7a71
James Wang <veriathon@gmail.com>
2017-03-10 01:56:23 -0800

    Adding useful functionality to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,9 +1,10 @@
 -- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
+k = hs.hotkey.modal.new({"ctrl"}, "f")
 
 launch = function(appname)
   hs.application.launchOrFocus(appname)
   k.triggered = true
+  k:exit()
 end
 
 -- Sequential keybindings, e.g. Hyper-a,f for Finder
@@ -28,24 +29,25 @@ releasedA = function() end
 k:bind({}, 'a', nil, pressedA, releasedA)
 
 -- Launch Alfred with HYPER+SHIFT+A
-k:bind({"shift"}, 'a', nil, function() launch('Alfred 3'); k.triggered = true; end)
+k:bind({"shift"}, 'a', nil, function() launch('Alfred 3'); end)
 
 -- Launch Emacs with HYPER+e
-k:bind({}, 'e', nil, function() launch('Emacs'); k.triggered = true; end)
+k:bind({}, 'e', nil, function() launch('Emacs'); end)
 
 -- Launch iTerm with HYPER+t
-k:bind({}, 't', nil, function() launch('iTerm'); k.triggered = true; end)
+k:bind({}, 't', nil, function() launch('iTerm'); end)
 
 -- Launch Chrome with HYPER+c
-k:bind({}, 'c', nil, function() launch('Google Chrome'); k.triggered = true; end)
+k:bind({}, 'c', nil, function() launch('Google Chrome'); end)
 
 -- Launch Slack with HYPER+s
-k:bind({}, 's', nil, function() launch('Slack'); k.triggered = true; end)
+k:bind({}, 's', nil, function() launch('Slack'); end)
 
 -- HYPER+up: Act like hyper up
 ufun = function()
   hs.eventtap.keyStroke({"shift", "cmd", "alt", "ctrl"}, "Up")
   k.triggered = true
+  k:exit()
 end
 k:bind({}, 'Up', nil, ufun)
 
@@ -53,6 +55,7 @@ k:bind({}, 'Up', nil, ufun)
 lfun = function()
   hs.eventtap.keyStroke({"shift", "cmd", "alt", "ctrl"}, "Left")
   k.triggered = true
+  k:exit()
 end
 k:bind({}, 'Left', nil, lfun)
 
@@ -60,6 +63,7 @@ k:bind({}, 'Left', nil, lfun)
 rfun = function()
   hs.eventtap.keyStroke({"shift", "cmd", "alt", "ctrl"}, "Right")
   k.triggered = true
+  k:exit()
 end
 k:bind({}, 'Right', nil, rfun)
 
@@ -67,23 +71,24 @@ k:bind({}, 'Right', nil, rfun)
 dfun = function()
   hs.eventtap.keyStroke({"shift", "cmd", "alt", "ctrl"}, "Down")
   k.triggered = true
+  k:exit()
 end
 k:bind({}, 'Down', nil, dfun)
 
 -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
+-- pressedF18 = function()
+--   k.triggered = false
+--   k:enter()
+-- end
 
 -- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
 --   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
+-- releasedF18 = function()
+--   k:exit()
+--   if not k.triggered then
+--     hs.eventtap.keyStroke({}, 'ESCAPE')
+--   end
+-- end
 
 -- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+-- f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
